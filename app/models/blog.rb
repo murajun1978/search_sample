6dd70391adb01912cc63c7ed390c824635e2dc97
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2013-03-26 01:09:46 +0900

    Search Add tag

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,10 @@
 class Blog < ActiveRecord::Base
   attr_accessible :comment
+  
+  LANG_RUBY = "ruby"
+  LANG_RAILS = "rails"
+  LANG_MOBIRUBY = "mobiruby"
+  LANG_RUBYMOTION = "rubymotion"
+  
+  LANG_SELECTION = [['Ruby', LANG_RUBY], ['Rails', LANG_RAILS], ['MobiRuby', LANG_MOBIRUBY], ['RubyMotion', LANG_RUBYMOTION]]
 end
